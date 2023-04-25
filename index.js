7658a38454841deb1d38031b551db4c53de14181
Faris-RS <brotofarisrs@gmail.com>
2023-04-25 11:46:02 +0530

    cors

--- index.js
+++ index.js
@@ -59,6 +59,11 @@ app.use(
     ],
     credentials: true,
     methods: "GET,POST,PUT,DELETE,PATCH",
+    allowedHeaders:[
+      'Content-type',
+      'Access',
+      'Authorization'
+  ]
   })
 );
 
