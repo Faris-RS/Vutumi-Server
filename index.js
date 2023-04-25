6c86bf928835b0cabcb6aef21b2fabb0aa868528
Faris-RS <brotofarisrs@gmail.com>
2023-04-25 11:36:56 +0530

    cors

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 import cookieSession from "cookie-session";
 import express from "express";
 import cors from "cors";
-import logger from 'morgan';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 import passportSetup from "./strategies/passport.js";
 import passport from "passport";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import fileUpload from "express-fileupload";
-import { Server } from "socket.io"; 
-import { v4 as uuidv4 } from 'uuid';
+import { Server } from "socket.io";
+import { v4 as uuidv4 } from "uuid";
 
 import authRouter from "./routes/auth.js";
 import connection from "./config/dbConnection.js";
-import userRouter from './routes/user.js';
-import postRouter from './routes/post.js'
-import adminRouter from './routes/admin.js';
-import chatRouter from './routes/chat.js'
+import userRouter from "./routes/user.js";
+import postRouter from "./routes/post.js";
+import adminRouter from "./routes/admin.js";
+import chatRouter from "./routes/chat.js";
 import userModel from "./models/userModel.js";
 
 const app = express();
@@ -32,35 +32,41 @@ app.use(
 );
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
-app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
-app.use(logger('dev'));
+app.use(
+  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
+);
+app.use(logger("dev"));
 app.use(cookieParser());
 app.use(fileUpload());
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(cors({
-  origin: function(origin, callback) {
-    if (origin === 'http://localhost:8080' || origin === 'http://localhost:4000') {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: "GET,POST,PUT,DELETE,PATCH",
-}));
+app.use(
+  cors({
+    // origin: function(origin, callback) {
+    //   if (origin === 'http://localhost:8080' || origin === 'http://localhost:4000') {
+    //     callback(null, true);
+    //   } else {
+    //     callback(new Error('Not allowed by CORS'));
+    //   }
+    // },
+    origin: [
+      "http://localhost:4000",
+      "http://http://vutumi-react.s3-website.ap-south-1.amazonaws.com/",
+    ],
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE,PATCH",
+  })
+);
 
 app.use("/auth", authRouter);
-app.use('/',userRouter);
-app.use('/post', postRouter)
-app.use('/admin',adminRouter)
-app.use('/chat', chatRouter)
-
-
+app.use("/", userRouter);
+app.use("/post", postRouter);
+app.use("/admin", adminRouter);
+app.use("/chat", chatRouter);
 
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
@@ -68,7 +74,10 @@ const server = app.listen(process.env.PORT, () =>
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:8080",
+    origin: [
+      "http://localhost:4000",
+      "http://http://vutumi-react.s3-website.ap-south-1.amazonaws.com/",
+    ],
     credentials: true,
   },
 });
@@ -77,13 +86,19 @@ global.onlineUsers = new Map();
 
 io.on("connection", (socket) => {
   // Search
-  socket.on('search', async(query) => {
+  socket.on("search", async (query) => {
     const results = {
-      data: await userModel.find({$or: [{firstName:{ $regex: query, '$options' : 'i' }}, {lastName:{ $regex: query, '$options' : 'i' }}]}).limit(5)
+      data: await userModel
+        .find({
+          $or: [
+            { firstName: { $regex: query, $options: "i" } },
+            { lastName: { $regex: query, $options: "i" } },
+          ],
+        })
+        .limit(5),
     };
-    socket.emit('searchResult', results);
+    socket.emit("searchResult", results);
   });
-
 
   // Chat
   global.chatSocket = socket;
@@ -112,9 +127,9 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("User Disconnected");
   });
-  
+
   let waitingRoom = null;
-  socket.on('joinWaitingRoom', () => {
+  socket.on("joinWaitingRoom", () => {
     console.log(waitingRoom);
     if (waitingRoom === null) {
       // Create a new waiting room
@@ -127,9 +142,8 @@ io.on("connection", (socket) => {
       console.log(`user ${socket.id} joined waiting room ${waitingRoom}`);
 
       // Notify the first user that the second user joined
-      io.to(waitingRoom).emit('startGame');
+      io.to(waitingRoom).emit("startGame");
       waitingRoom = null; // Reset waiting room
     }
   });
-
 });
