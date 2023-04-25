8dc7d05fb3538b8e6e1c096bb4f7bd6f8658ebb1
Faris-RS <brotofarisrs@gmail.com>
2023-04-25 11:05:17 +0530

    hosting commit

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const connection = ()=>{
-    // mongoose.connect('mongodb://0.0.0.0:27017/vutumi5')
-    mongoose.connect('mongodb+srv://vutumi:0000@cluster0.beoalfy.mongodb.net/vutumi?retryWrites=true&w=majority')
+    // mongoose.connect('mongodb://0.0.0.0:27017/vutumi')
+    mongoose.connect(process.env.DATABASE)
 }
 mongoose.set('strictQuery', true);
 
